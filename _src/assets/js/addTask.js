abbd4adc9a16208dd74bca567687633b19835869
elviac94 <elvi_ac94@hotmail.com>
2020-05-05 16:44:08 +0200

    add text card

--- _src/assets/js/addTask.js
+++ _src/assets/js/addTask.js
@@ -6,18 +6,42 @@ const listContainer = document.querySelectorAll('.app-list')// contenedor del fo
 listContainer.forEach(list => {
     const contenedorArticles = list.querySelector('.p-1.rounded-sm.bg-primary.shadow');
     const buttonAddTask = list.querySelector('.ml-1.btn.btn-primary.btn-sm.text-white-50')//boton//
-    buttonAddTask.addEventListener('click', addNewCard(contenedorArticles,buttonAddTask))
-    
+    buttonAddTask.addEventListener('click', addNewCard(contenedorArticles, buttonAddTask))
+
 
 });
 
-function addNewCard(contenedor,buttonAddTask){
+function addNewCard(contenedor, buttonAddTask) {
     return () => {
-        const newCard= document.createElement('article');
-        newCard.setAttribute('class','js-card app-card m-1 mb-2 p-2 bg-white rounded-sm app-cursor-pointer shadow-sm');
+        const newCard = document.createElement('article');
+        const cardSpanContainer = document.createElement('div');
+        const cardTitle = document.createElement('h3');
+        const spanCardOne = document.createElement('span');
+        const spanCardTwo = document.createElement('span');
+        const spanCardThree = document.createElement('span');
+        spanCardOne.setAttribute('class','badge badge-secondary bg-success');
+        spanCardTwo.setAttribute('class','badge badge-secondary bg-success');
+        spanCardThree.setAttribute('class','badge badge-secondary bg-success');
+        cardTitle.setAttribute('class','h6');
+        const textJS=document.createTextNode('JS');
+        const textCss=document.createTextNode('Css');
+        const textHtml=document.createTextNode('Html');
+        const textTitle=document.createTextNode('Necesito texto');
+        cardTitle.appendChild(textTitle);
+        spanCardOne.appendChild(textJS);
+        spanCardTwo.appendChild(textCss);
+        spanCardThree.appendChild(textHtml);
+        cardSpanContainer.appendChild(spanCardOne);
+        cardSpanContainer.appendChild(spanCardTwo);
+        cardSpanContainer.appendChild(spanCardThree);
+        cardSpanContainer.appendChild(cardTitle);
+        newCard.setAttribute('class', 'js-card app-card m-1 mb-2 p-2 bg-white rounded-sm app-cursor-pointer shadow-sm');
+        newCard.setAttribute('title', 'Abrir la tarjeta');
+        newCard.appendChild(cardSpanContainer);// añado el div con los span//
+        newCard.appendChild(cardSpanContainer);// añado el div con el title//
         contenedor.appendChild(newCard);
 
-        contenedor.insertBefore(newCard,buttonAddTask)
+        contenedor.insertBefore(newCard, buttonAddTask)
     }
 }
 
