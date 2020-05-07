cdc5f445ec3a1e8b816a5380569e5dedf62df9fa
elviac94 <elvi_ac94@hotmail.com>
2020-05-07 10:35:19 +0200

    prevent openning the modal by clicking on the up or down button

--- _src/assets/js/moveTask.js
+++ _src/assets/js/moveTask.js
@@ -4,18 +4,18 @@ const allTasks = document.querySelectorAll('.js-card.app-card.m-1.mb-2.p-2.bg-wh
 
 const moveUp = (card, event) => {
     card.parentElement.insertBefore(card, card.previousElementSibling);
-    event.preventDefault();
+    event.stopPropagation();
 }
 
 const moveDown = (card, event) => {
     card.parentElement.insertBefore(card.nextElementSibling, card);
-    event.preventDefault();
+    event.stopPropagation();
 }
 
 const moveTask = (card) => {
     const buttonUp = card.querySelector('.btn.btn-light.text-muted.border.shadow-sm.app-card-move-up');
     const buttonDown = card.querySelector('.btn.btn-light.text-muted.border.shadow-sm.app-card-move-down');
-    buttonUp.addEventListener('click', (event) => moveUp(card, event) );
+    buttonUp.addEventListener('click', (event) => moveUp(card, event));
     buttonDown.addEventListener('click', (event) => moveDown(card, event));
 }
 
